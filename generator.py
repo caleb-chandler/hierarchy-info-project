53ee5bb8824162ab4c099726b51dad59182febc0
Caleb Chandler <chandlerc41eb@gmail.com>
2026-03-22 16:32:38 -0400

    first draft of generator

--- generator.py
+++ generator.py
@@ -17,7 +17,7 @@ def generator(type, C, N, weight_dist=None, **kwargs):
     type : str
         Must be either:
             - "control" (G,n,p random)
-            - "hierarchy" (Tree with branching factor C, descending weights of decay Wd, and
+            - "hierarchy" (Tree with branching factor C, descending weights of step size S, and
             further edges probabilistically added between leaves based on relatedness to bring
             mean degree in line with C)
             - "alternative" (Custom degree-controlled SBM with # communities
@@ -36,12 +36,12 @@ def generator(type, C, N, weight_dist=None, **kwargs):
         - np.random.pareto (a = 2)
         - halfnorm.rvs (scale = 5)
         - build_alt (i_prob = 0.2, o_prob = 0.8, n_comms = 8)
-        - build_tree (Wd = 'linear')
+        - build_tree (S = 1)
 
     Returns
     ---
     graph : (N, N) row-stochastic array
-        NormalizPd weightPd adjacency matrix.
+        Normalized weighted adjacency matrix.
     '''
     # unpacking kwargs
     a = kwargs.get('a', 2)
@@ -49,7 +49,7 @@ def generator(type, C, N, weight_dist=None, **kwargs):
     i_prob = kwargs.get('i_prob', 0.8)
     o_prob = kwargs.get('o_prob', 0.2)
     n_comms = kwargs.get('n_comms', 8)
-    Wd = kwargs.get('Wd', 'linear')
+    S = kwargs.get('S', 1)
 
     # dynamically setting std dev of deg dist
     deg_sigma = C/30 if C/30 >= 1 else 1
@@ -74,7 +74,7 @@ def generator(type, C, N, weight_dist=None, **kwargs):
         elif type == 'alternative':
             def build_alt(i_prob=i_prob, o_prob=o_prob, n_comms=n_comms):
                 '''
-                Implements degree-correctPd SBM to enforce weight distribution
+                Implements degree-corrected SBM to enforce weight distribution
                 '''
                 # calculate expected degrees
                 exp_deg = np.random.normal(C, deg_sigma, N)
@@ -100,16 +100,17 @@ def generator(type, C, N, weight_dist=None, **kwargs):
             return None
     
     else:
-        def build_tree(D):
+        def build_tree(S):
             h = math.log(N, C) # determine height based on N
+            b = C-1 # branching factor
             # creating as DiGraph for easier lookup
-            G = nx.balanced_tree(C, h, create_using=nx.DiGraph())
+            G = nx.balanced_tree(b, h, create_using=nx.DiGraph())
 
             # leaves have deg 0 in directed case
             leaves = {n for n, degree in G.out_degree() if degree == 0}
             branches = {n for n, degree in G.out_degree() if degree > 0}
 
-            # --- branch -> leaf descendants + node -> level dicts ---
+            # --- branch:leaf descendants + node:level dicts ---
 
             # recursive function to find all leaf descendants
             memo = {}
@@ -134,15 +135,47 @@ def generator(type, C, N, weight_dist=None, **kwargs):
 
             # node-to-level
             levels_dict = nx.shortest_path_length(G, source=0)
-            nodes_at_level = defaultdict(list)
-            for node, level in levels_dict.items():
-                nodes_at_level[node].append(level)
+            # reversing order so that probability calculation works correctly
+            max_dist = max(levels_dict.values())
+            nodes_to_level = {node: (max_dist - dist) for node, dist in levels_dict.items()}
 
             # --- adding new edges between leaves ---
 
             # determining probabilities
-            Pd = # probability decay parameter (controls shape of distribution)
+            Pd = 0.5 # probability decay parameter (controls shape of distribution)
             def edge_prob(i, j):
                 LCA = nx.lowest_common_ancestor(G, i, j)
-                LCA_level = nodes_at_level[LCA]
+                LCA_level = nodes_to_level[LCA]
                 return Pd**LCA_level
+            
+            # adding only enough edges to match density
+            counter = 0
+            for u, v in combinations(leaves, 2):
+                if G.has_edge(u, v) == False: # skip existing edges
+                    if edge_prob(u, v) < random.random():
+                        G.add_edge(u, v)
+                        counter += 1
+                if counter == b**h/2:
+                    break
+
+            # extracting adj matrix
+            adj = nx.to_numpy_array(G, nodelist=sorted(G.nodes())) # ensuring order
+
+            # --- applying weights ---
+
+            # weight-to-level dict
+            level_to_weight = {}
+            current_weight = 1
+            for level in sorted(set(nodes_to_level.values())): # reversed the order previously, so it should be leaves -> root
+                level_to_weight[level] = current_weight
+                current_weight += S
+
+            # assign node weights accordingly
+            node_to_weight = {}
+            for node, level in nodes_to_level.items():
+                node_to_weight[node] = level_to_weight[level]
+            
+            # convert to array (sorting by node for consistency with adj)
+            weights = np.array([w for n, w in sorted(node_to_weight.items())])
+
+            return build_weight_matrix(adj, weights)
